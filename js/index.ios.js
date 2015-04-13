5afa1a00cef78765812c27c69f16a82b202a55f8
David Hsu <dvdhsu@gmail.com>
2015-04-12 18:55:21 -0700

    Remove extraneous console.log statements in index.

--- js/index.ios.js
+++ js/index.ios.js
@@ -24,13 +24,10 @@ class NotifstaLaunch extends React.Component {
   renderScene(route, nav) {
     switch(route.id) {
       case 'Login':
-        console.log("transitioning to login");
         return <Login navigator={nav}/>;
       case 'Event':
-        console.log("transitioning to event");
         return <Event navigator={nav} email={route.email} token={route.token} eventId={route.eventId}/>
       case 'EventList':
-        console.log("transitioning to event list");
         return <EventList navigator={nav} email={route.email} token={route.token} events={route.events}/>
     }
   }
