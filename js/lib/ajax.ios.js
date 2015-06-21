a45dce7ff529e5322c743af2aff98710b4c999c7
David Hsu <dvdhsu@gmail.com>
2015-06-20 20:21:44 -0700

    Encode passwords and emails.
    Previously, passwords weren't encoded. So, some logins would fail (e.g. password
    contains "#"). This commit fixes the issue by encoding the password with a
    `encodeURIComponent'.
    
    The same fix is applied to emails.

--- js/lib/ajax.ios.js
+++ js/lib/ajax.ios.js
@@ -18,7 +18,7 @@ var {
 } = React;
 
 exports.login = function(email, password) {
-  requestUrl = API_BASE + 'auth/login/?email=' + email + '&password=' + password + '&ios=true';
+  requestUrl = API_BASE + 'auth/login/?email=' + encodeURIComponent(email) + '&password=' + encodeURIComponent(password) + '&ios=true';
   return(
     fetch(requestUrl)
       .then((unparsed) => unparsed.json())
@@ -26,7 +26,7 @@ exports.login = function(email, password) {
 }
 
 exports.register = function(email, password) {
-  requestUrl = API_BASE + 'auth/register/?email=' + email + '&password=' + password + '&ios=true';
+  requestUrl = API_BASE + 'auth/register/?email=' + encodeURIComponent(email) + '&password=' + encodeURIComponent(password) + '&ios=true';
   return(
     fetch(requestUrl, { method: 'post' })
       .then((unparsed) => unparsed.json())
@@ -34,7 +34,7 @@ exports.register = function(email, password) {
 }
 
 exports.loginWithToken = function(email, token) {
-  requestUrl = API_BASE + 'auth/login_with_token/?email=' + email + '&token=' + token + '&ios=true';
+  requestUrl = API_BASE + 'auth/login_with_token/?email=' + encodeURIComponent(email) + '&token=' + token + '&ios=true';
   return(
     fetch(requestUrl)
       .then((unparsed) => unparsed.json())
@@ -42,7 +42,7 @@ exports.loginWithToken = function(email, token) {
 }
 
 exports.facebookCreateOrLogin = function(email, facebookId, facebookToken) {
-  requestUrl = API_BASE + 'auth/facebook/?email=' + email + '&facebook_id=' + facebookId + '&facebook_token=' + facebookToken + '&ios=true';
+  requestUrl = API_BASE + 'auth/facebook/?email=' + encodeURIComponent(email) + '&facebook_id=' + facebookId + '&facebook_token=' + facebookToken + '&ios=true';
 
   return(
     fetch(requestUrl)
@@ -57,7 +57,7 @@ exports.facebookCreateOrLogin = function(email, facebookId, facebookToken) {
 }
 
 exports.getEvent = function(email, token, eventId) {
-  requestUrl = API_BASE + 'events/' + eventId + '/?user_email=' + email + '&user_token=' + token;
+  requestUrl = API_BASE + 'events/' + eventId + '/?user_email=' + encodeURIComponent(email) + '&user_token=' + token;
   return(
     fetch(requestUrl)
       .then((unparsed) => unparsed.json())
@@ -68,7 +68,7 @@ exports.getEvent = function(email, token, eventId) {
 }
 
 exports.getNotifications = function(email, token, channelId) {
-  requestUrl = API_BASE + 'channels/' + channelId + '/notifications/?user_email=' + email + '&user_token=' + token;
+  requestUrl = API_BASE + 'channels/' + channelId + '/notifications/?user_email=' + encodeURIComponent(email) + '&user_token=' + token;
   return(
     fetch(requestUrl)
       .then((unparsed) => unparsed.json())
@@ -79,7 +79,7 @@ exports.getNotifications = function(email, token, channelId) {
 }
 
 exports.subscribe = function(email, token, eventId) {
-  requestUrl = API_BASE + 'subscriptions/?user_email=' + email + '&user_token=' + token + '&event_id=' + eventId;
+  requestUrl = API_BASE + 'subscriptions/?user_email=' + encodeURIComponent(email) + '&user_token=' + token + '&event_id=' + eventId;
   return(
     fetch(requestUrl, { method: 'post' })
       .then((unparsed) => unparsed.json())
@@ -90,7 +90,7 @@ exports.subscribe = function(email, token, eventId) {
 }
 
 exports.unsubscribe = function(email, token, eventId) {
-  requestUrl = API_BASE + '/events/' + eventId + '/subscription/?user_email=' + email + '&user_token=' + token;
+  requestUrl = API_BASE + '/events/' + eventId + '/subscription/?user_email=' + encodeURIComponent(email) + '&user_token=' + token;
   return(
     fetch(requestUrl, { method: 'delete' })
       .then((unparsed) => unparsed.json())
@@ -101,7 +101,7 @@ exports.unsubscribe = function(email, token, eventId) {
 }
 
 exports.sendNotification = function(email, token, channelId, notificationGuts) {
-  requestUrl = API_BASE + 'channels/' + channelId + '/notifications/?user_email=' + email +
+  requestUrl = API_BASE + 'channels/' + channelId + '/notifications/?user_email=' + encodeURIComponent(email) +
     '&user_token=' + token + '&notification[notification_guts]=' + encodeURIComponent(notificationGuts) + '&notification[type]=Message'
   console.log(requestUrl);
   return(
